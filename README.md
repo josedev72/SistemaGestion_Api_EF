b3b333dd8f40855dc999c4c45f7c25fb10a902d2
Jose Gonzalez <cobranzasdiariodecuyosj@gmail.com>
2024-06-22 13:09:18 -0300

    Actualizado de Readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# SistemaGestion_v3
+# SistemaGestion
+##   - API, para persistencia con SQL Server
+##   - Web, para consumo de la Api
+
+####**Nota:** ** * * IDE utilizado, Visual Studio 2022 + SSMS 2019 * * ** 
--- SistemaGestion.Cliente/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaGestion.Cliente.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
